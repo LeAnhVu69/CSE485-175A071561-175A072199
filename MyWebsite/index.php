46775bb495fbe6994f177587aedd5b6ef9726fb4
BuiVanHuy59 <54974839+BuiVanHuy59@users.noreply.github.com>
2019-10-10 23:27:42 +0700

    Add files via upload

--- MyWebsite/index.php
+++ MyWebsite/index.php
@@ -5,48 +5,14 @@
     <meta content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" name="viewport">
     <title>Trường Đại Học Nguyễn Tất Thành - NTTU</title>
     <script src="js/script.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/style2.css">
-    <link rel="stylesheet" href="css/slider.css">
-    <link rel="stylesheet" href="slick-1.8.1/slick/slick.css">
-    <link rel="stylesheet" href="slick-1.8.1/slick/slick-theme.css">
+
+   
+    
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap/js/bootstrap.js">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css">
+
 </head>
-<body>
-    <header class="top-header">
-        <?php
-            include ('header.php');
-        ?>
-    </header>
-
-    <?php
-        include ('main-header.php');
-    ?>
-
-    <?php
-        include ('menu.php');
-    ?>
-
-<main>
-    <div class="container">
-        <div class="row">
-            <section id="content">
-                <?php
-                    include ('main-slideshow.php');
-                ?>
-            <div class="d-md-flex section">
-                <?php
-                    include ('news1-tintuc.php');
-                ?>
-                <?php
-                    include ('news1-media.php');
-                ?>
-            </div>
-                <?php
-                    include ('sub-slideshow1.php');
-                ?>
+    
 
             <div class="section d-md-flex border-top">
                 <div class="col-lg-8">
@@ -68,17 +34,17 @@
         <?php
             include ('sub-slideshow2.php');
         ?>
-        </div> <!-- row -->
-    </div> <!-- container -->
-</main>
+        </div>
+    </div> 
+
 
 <footer>
     <?php
-        include ('top-foot.php');
+        include ('foottop.php');
     ?>
 
     <?php
-        include ('bot-foot.php');
+        include ('footbot.php');
     ?>
 </footer>
 
@@ -95,13 +61,7 @@
             autoplaySpeed: 2000,
             pauseOnHover: false
         })
-        // $('.multi-slide').slick({
-        //     infinite: true,
-        //     slidesToShow: 3,
-        //     slidesToScroll: 1
-        // })
-    })
+        
 </script>
 
 </body>
-</html>
