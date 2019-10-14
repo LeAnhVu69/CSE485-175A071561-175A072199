4a69ac71f3ccb0e7eb31833348c3890fcdd25b3e
LeAnhVu69 <54577732+LeAnhVu69@users.noreply.github.com>
2019-10-15 03:50:09 +0700

    Update

--- MyWebsite/process-register-page.php
+++ MyWebsite/process-register-page.php
@@ -1,69 +1,51 @@
 <?php
-try {
-	$errors = array();                       
-        $first_name = filter_var( $_POST['first_name'], FILTER_SANITIZE_STRING);	
-	if (empty($first_name)) {
-		$errors[] = 'Bạn chưa nhập tên';
-	}
-	    $last_name = filter_var( $_POST['last_name'], FILTER_SANITIZE_STRING);	
-	if (empty($last_name)) {
-		$errors[] = 'Bạn chưa nhập họ';
-	}
-	    $email = filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL);	
-	if  ((empty($email)) || (!filter_var($email, FILTER_VALIDATE_EMAIL))) {
-		$errors[] = 'Bạn chưa nhập email';
-		$errors[] = 'Hoặc email đã nhập sai';
-	}
-			$password1 = filter_var( $_POST['password1'], FILTER_SANITIZE_STRING);
-			$password2 = filter_var( $_POST['password2'], FILTER_SANITIZE_STRING);
-	if (!empty($password1)) {
-		if ($password1 !== $password2) { 
-			$errors[] = 'Mật khẩu không khớp';
-		} 
-	} else {
-		$errors[] = 'Bạn chưa nhập mật khẩu';
-	}
-	if (empty($errors)) {
-	    $hashed_passcode = password_hash($password1, PASSWORD_DEFAULT); 
-		require ('mysqli_connect.php'); 
-		$query = "INSERT INTO users (userid, first_name, last_name, email, password, registration_date) ";
-		$query .="VALUES(' ', ?, ?, ?, ?, NOW() )";		                
-        $q = mysqli_stmt_init($dbcon);                                  
-        mysqli_stmt_prepare($q, $query);
-        mysqli_stmt_bind_param($q, 'ssss', $first_name, $last_name, $email, $hashed_passcode);
-        mysqli_stmt_execute($q);
-        if (mysqli_stmt_affected_rows($q) == 1) {			
-		header ("location: register-thanks.php"); 
-		exit();
-		} else { 
-		    $errorstring = "<p class='text-center col-sm-8' style='color:red'>";
-			$errorstring .= "Hệ thống xảy ra lỗi <br> Bạn đã đăng ký không thành công";
-			$errorstring .= "Chúng tôi xin lỗi vì sự bất tiện này</p>"; 
-			echo "<p class=' text-center col-sm-2' style='color:red'>$errorstring</p>";
-			// Debugging message below do not use in production
-			//echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $query . '</p>';
-		    mysqli_close($dbcon); // Close the database connection.
-			echo '<footer class="jumbotron text-center col-sm-12"
-	        style="padding-bottom:1px; padding-top:8px;">
-            include("footer.php"); 
-            </footer>';
-		exit();
-		}
-	} else {                            
-		$errorstring = "Lỗi! <br> Các lỗi sau đã xảy ra:<br>";
-		foreach ($errors as $msg) {
-			$errorstring .= " - $msg<br>\n";
-		}
-		$errorstring .= "Xin thử lại sau<br>";
-		echo "<p class=' text-center col-sm-2' style='color:red'>$errorstring</p>";
-		}// End of if (empty($errors)) IF.
-}									
-   catch(Exception $e)
-   {
-     print "Hệ thống bận xin thử lại sau";
-   }
-   catch(Error $e)
-   {
-      print "Hệ thống bận xin thử lại sau";
-   }
-?>
+session_start();
+$first_name = "";
+$last_name = "";
+$email = "";
+$conn = mysqli_connect('localhost', 'root', '', 'moitinh');
+if (!$conn){
+	die("Khong the ket noi. Xin nhap lai!");
+}
+
+if (isset($_POST['submit'])) {
+  // receive all input values from the form
+  $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+  $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $password1 = mysqli_real_escape_string($conn, $_POST['password1']);
+  $password2 = mysqli_real_escape_string($conn, $_POST['password2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($first_name)) { array_push($errors, "Phải nhập tên"); }
+  if (empty($last_name)) { array_push($errors, "Phải nhập họ"); }
+  if (empty($email)) { array_push($errors, "Phải nhập email"); }
+  if (empty($password1)) { array_push($errors, "Phải nhập mật khẩu"); }
+  if ($password1 != $password2) {
+	array_push($errors, "Hai mật khẩu không trùng nhau");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($conn, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['email'] === $email) {
+      array_push($errors, "Email đã được đăng ký");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password1);//encrypt the password before saving in the database
+	$query = "INSERT INTO users (userid, first_name, last_name, email, password, registration_date)
+	VALUES('', '$first_name', '$last_name', '$email', '$password', NOW()) ";
+  	mysqli_query($conn, $query);
+  	$_SESSION['email'] = $email;
+  	$_SESSION['success'] = "Bạn đã đăng nhập";
+  	header('location: index.php');
+  }
+}
